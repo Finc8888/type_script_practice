a36d683d5a5e4e8870b65126e3c41a0367c1c15f
vprikhodko <vprikhodko@dunrose.ru>
2020-07-11 19:01:21 +0300

    feat(ts):Завершение добавления логики по фабрике классов

--- pattetnFactoryTest.js
+++ pattetnFactoryTest.js
@@ -1,24 +1,19 @@
-namespace patternFactoryTest{
-    enum PersonCategory{
-        Infant,
-        Child,
-        Adult,
-        Undefinded
-    }
-    interface IPerson {
-        Category: PersonCategory;
-        canSignContracts(): boolean;
-        printDetailInfo(): void;
-    }
-    abstract class Person implements IPerson{
-        Category: PersonCategory;
-        private DateOfBirthday: Date;
-        protected constructor(DateOfBirthday: Date) {
+"use strict";
+var patternFactoryTest;
+(function (patternFactoryTest) {
+    let PersonCategory;
+    (function (PersonCategory) {
+        PersonCategory[PersonCategory["Infant"] = 0] = "Infant";
+        PersonCategory[PersonCategory["Child"] = 1] = "Child";
+        PersonCategory[PersonCategory["Adult"] = 2] = "Adult";
+        PersonCategory[PersonCategory["Undefinded"] = 3] = "Undefinded";
+    })(PersonCategory || (PersonCategory = {}));
+    class Person {
+        constructor(DateOfBirthday) {
             this.DateOfBirthday = DateOfBirthday;
             this.Category = PersonCategory.Undefinded;
         }
-        abstract canSignContracts():boolean;
-        printDetailInfo(){
+        printDetailInfo() {
             let detailInfo = `
                 Detail Info for Person:
                 Date of Birthday: ${this.DateOfBirthday.toDateString()}
@@ -28,45 +23,44 @@ namespace patternFactoryTest{
             console.info(detailInfo);
         }
     }
-    class Infant extends Person{
-        constructor(dateOfBirthday: Date) {
+    class Infant extends Person {
+        constructor(dateOfBirthday) {
             super(dateOfBirthday);
             this.Category = PersonCategory.Infant;
         }
-        canSignContracts(): boolean {
+        canSignContracts() {
             return false;
         }
-
     }
-    class Child extends Person{
-        constructor(dateOfBirthday: Date){
+    class Child extends Person {
+        constructor(dateOfBirthday) {
             super(dateOfBirthday);
             this.Category = PersonCategory.Child;
         }
-        canSignContracts(): boolean {
+        canSignContracts() {
             return false;
         }
     }
-    class Adult extends Person{
-        constructor(dateOfBirthday: Date) {
+    class Adult extends Person {
+        constructor(dateOfBirthday) {
             super(dateOfBirthday);
             this.Category = PersonCategory.Adult;
         }
-        canSignContracts(): boolean {
+        canSignContracts() {
             return true;
         }
     }
-    class PersonFactory{
-        getPerson(dateOfBirthday: Date):IPerson{
+    class PersonFactory {
+        getPerson(dateOfBirthday) {
             let currentDate = new Date();
             let currentMonth = currentDate.getMonth() + 1;
             let currentDay = currentDate.getDay();
-            let dateTwoYearsAgo = new Date(currentDate.getFullYear() - 2,currentMonth,currentDay);
-            let date18YearsAgo = new Date(currentDate.getFullYear() - 18,currentMonth,currentDay);
-            if(dateOfBirthday >= dateTwoYearsAgo){
+            let dateTwoYearsAgo = new Date(currentDate.getFullYear() - 2, currentMonth, currentDay);
+            let date18YearsAgo = new Date(currentDate.getFullYear() - 18, currentMonth, currentDay);
+            if (dateOfBirthday >= dateTwoYearsAgo) {
                 return new Infant(dateOfBirthday);
             }
-            else if(dateOfBirthday >= date18YearsAgo){
+            else if (dateOfBirthday >= date18YearsAgo) {
                 return new Child(dateOfBirthday);
             }
             return new Adult(dateOfBirthday);
@@ -75,10 +69,8 @@ namespace patternFactoryTest{
     let factory = new PersonFactory();
     let person1 = factory.getPerson(new Date(2019, 0, 20));
     person1.printDetailInfo();
-
     let person2 = factory.getPerson(new Date(2010, 0, 20));
     person2.printDetailInfo();
-
     let person3 = factory.getPerson(new Date(1988, 0, 20));
     person3.printDetailInfo();
-}
+})(patternFactoryTest || (patternFactoryTest = {}));
